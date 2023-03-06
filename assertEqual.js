e319c057aa9d1547cd25d6b47c90b3ecd9a1105e
Takuya <takuya.k.toyokawa@protonmail.com>
2023-03-06 14:55:47 -0800

    finished assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  let message = "Error producing assertion";
+
+  if (actual === expected) {
+    message = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  }
+
+  if (actual !== expected) {
+    message = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+
+  console.log(message);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
+assertEqual(1, 1);
