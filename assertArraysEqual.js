5e795be743c736a5369ee20d2fcdbdb87efec50d
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-07 15:06:29 -0800

    fixed assertarrays equal based on requirements + without init

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,28 @@
-const assertArraysEqual = (arr1, arr2) => {
+const eqArrays = (arr1, arr2) => {
   // Loop through both arr1 and arr2
   for (let i = 0; i < arr1.length; i++) {
     // If one indexed element do not match, return false and break
     if (arr1[i] !== arr2[i]) {
-      return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
+      return false;
     }
   }
-
   // Return true if every single element passed
-  return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
+  return true;
 };
 
-console.log(
-  assertArraysEqual([1, 2, 3], [1, 2, "3"]) // => should FAIL
-);
+const assertArraysEqual = (arr1, arr2) => {
+  let verify = eqArrays(arr1, arr2);
+  let message = "Error producing assertion";
+
+  if (verify) {
+    message = `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
+  }
+
+  if (!verify) {
+    message = `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
+  }
+
+  console.log(message);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, "3"]); // => should FAIL
---
+++ without.js
@@ -0,0 +1,18 @@
+const without = (arr1, arr2) => {
+
+  let
+  // Loop through both arr1 and arr2
+  for (let i = 0; i < arr1.length; i++) {
+    // If one indexed element do not match, return false and break
+    if (arr1[i] !== arr2[i]) {
+      return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
+    }
+  }
+
+  // Return true if every single element passed
+  return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
+};
+
+console.log(
+  assertArraysEqual([1, 2, 3], [1, 2, "3"]) // => should FAIL
+);
