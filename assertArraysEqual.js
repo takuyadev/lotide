15f8a167e6028819c9bf79d6d3169614deb77007
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-07 15:34:58 -0800

    fixed eqArrays to match for mismatch lengths + without finish

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,9 @@
 const eqArrays = (arr1, arr2) => {
+  // If both arrays are different lengths, they are also false
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
   // Loop through both arr1 and arr2
   for (let i = 0; i < arr1.length; i++) {
     // If one indexed element do not match, return false and break
--- without.js
+++ without.js
@@ -1,18 +1,50 @@
-const without = (arr1, arr2) => {
+const eqArrays = (arr1, arr2) => {
+  
+  // If both arrays are different lengths, they are also false
+  if (arr1.length !== arr2.length){
+    return false
+  }  
 
-  let
   // Loop through both arr1 and arr2
-  for (let i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < longestLength; i++) {
     // If one indexed element do not match, return false and break
     if (arr1[i] !== arr2[i]) {
-      return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
+      return false;
     }
+  }
+  // Return true if every single element passed
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  let verify = eqArrays(arr1, arr2);
+  let message = "Error producing assertion";
+
+  if (verify) {
+    message = `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
+  }
+
+  if (!verify) {
+    message = `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
+  }
+
+  console.log(message);
+};
+
+const without = (source, itemsToRemove) => {
+  let newArr = [...source];
+
+  // Loop through both arr1 and arr2
+  for (let i = 0; i < itemsToRemove.length; i++) {
+    newArr = newArr.filter((item) => item !== itemsToRemove[i]);
   }
 
   // Return true if every single element passed
-  return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
+  return newArr;
 };
 
-console.log(
-  assertArraysEqual([1, 2, 3], [1, 2, "3"]) // => should FAIL
-);
+const words = ["hello", "world", "lighthouse"];
+const newwords = without(words, ["lighthouse"]); // no need to capture return value for this test case
+console.log(words);
+// Make sure the original array was not altered by the without function
+assertArraysEqual(newwords, ["hello", "world", "lighthouse"]);
