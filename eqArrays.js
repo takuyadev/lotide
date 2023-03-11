0b3a35b78ac7220a7b45768714bd3d1ecae4deff
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-10 17:06:44 -0800

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -21,6 +21,13 @@ const eqArrays = (arr1, arr2) => {
   // Loop through both arr1 and arr2
   for (let i = 0; i < arr1.length; i++) {
     // If one indexed element do not match, return false and break
+    if (Array.isArray(arr1[i])) {
+      if (eqArrays(arr1[i], arr2[i])) {
+        continue;
+      }
+      return false;
+    }
+
     if (arr1[i] !== arr2[i]) {
       return false;
     }
@@ -31,4 +38,7 @@ const eqArrays = (arr1, arr2) => {
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // => should FAIL
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
