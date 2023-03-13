b1d8085ab936aa6be398428c7e875aee78262b93
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-13 09:38:53 -0700

    refctor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-const eqArrays = (arr1, arr2) => {
-  // If both arrays are different lengths, they are also false
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // Loop through both arr1 and arr2
-  for (let i = 0; i < arr1.length; i++) {
-    // If one indexed element do not match, return false and break
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  // Return true if every single element passed
-  return true;
-};
+const eqArrays = require("./eqArrays")
 
 const assertArraysEqual = (arr1, arr2) => {
   let verify = eqArrays(arr1, arr2);
@@ -30,4 +15,4 @@ const assertArraysEqual = (arr1, arr2) => {
   console.log(message);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, "3"]); // => should FAIL
+module.exports = assertArraysEqual;
