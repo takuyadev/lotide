09765226ee3cc003187f4fdf5d24406be6e75a31
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-13 10:56:26 -0700

    created module exports file

---
+++ index.js
@@ -0,0 +1,39 @@
+// index.js
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const lettersPosition = require("./lettersPositions");
+const map = require("./map");
+const middle = require("./middle");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  lettersPosition,
+  map,
+  takeUntil,
+  without
+};
--- test/middleTest.js
+++ test/middleTest.js
@@ -16,9 +16,9 @@ describe("eqArrays()", () => {
   });
 
   it("should return [3] when passed [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]", () => {
-    expect(middle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])).to.deep.equal([4, 5]);
+    // expect(middle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])).to.deep.equal([4, 5]);
+    assert.deepEqual(middle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), [4, 5]);
   });
-
 });
 
 // assertArraysEqual(middle([0, 1, 2, 3, 4, 5, 6]), [3]);
